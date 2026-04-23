74a0ae0fa190f991b0ad3a465f6c5f12ea550575
Philipp <phipzzz@mail.de>
2026-04-23 12:42:18 +0200

    Improve error handling (#3)
    - Improve error handling
    - Don't stop the service if a request fails (#2)
    - Adapr default restart policy (#1)
    - Update devcontainer.json
    - update docu
    - Fix error messages

--- main.go
+++ main.go
@@ -25,7 +25,7 @@ func main() {
 
 			if err != nil {
 				logger.Error("Error executing command", "err", err)
-				os.Exit(0)
+				os.Exit(1)
 			}
 
 			fmt.Println("Firewalls:")
@@ -37,53 +37,52 @@ func main() {
 
 		default:
 			logger.Error("unknown command", "cmd", os.Args[1])
-			os.Exit(0)
+			os.Exit(1)
 		}
 	}
 
 	if os.Getenv("HCLOUD_TOKEN") == "" {
 		logger.Error("No HCLOUD_TOKEN provided. Stopping service...")
-		os.Exit(0)
+		os.Exit(1)
 	}
 
 	if os.Getenv("IP_MODE") == "" {
 		logger.Error("No IP_MODE provided. Stopping service...")
-		os.Exit(0)
+		os.Exit(1)
 	}
 
 	cfg, err := config.NewConfig(logger)
 	if err != nil {
 		logger.Error("Stopping service due to errors in the config file", "err", err)
-		os.Exit(0)
+		os.Exit(1)
 	}
 	logger.Debug("Config", "cfg", cfg)
 
 	ipc, err := pubip.NewIPChecker(logger)
   	if err != nil {
 		logger.Error("Error creating IPChecker", "err", err)
-		os.Exit(0)
+		os.Exit(1)
     }
 
 	upd, err := internal.NewUpdater(logger, cfg, ipc)
 	if err != nil {
 		logger.Error("Error creating Updater", "err", err)
-		os.Exit(0)
+		os.Exit(1)
     }
 
 	i, err := strconv.ParseInt(os.Getenv("UPDATE_INTERVAL"), 10, 64)
     if err != nil {
 		logger.Error("Invalid UPDATE_INTERVAL", "err", err)
-		os.Exit(0)
+		os.Exit(1)
     }
 
 	success := false
 
 	for true {
 		changed, err := ipc.CheckForChanges()
-
 		if err != nil {
 			logger.Error("Stopping service due to errors", "err", err)
-			os.Exit(0)
+			os.Exit(1)
 		}
 		
 		if changed || !success {
